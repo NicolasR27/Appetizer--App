6008131b3ced5c4529e24964512586077af990f9
Nicolas Rios <nickrios33@yahoo.com>
2021-02-01 00:35:30 -0800

    added loading indicator

--- Appetizer -App/AppetizerTabView.swift
+++ Appetizer -App/AppetizerTabView.swift
@@ -24,7 +24,7 @@ struct AppetizerTabView: View {
                 }
             
         }
-        .accentColor(Color("brandPrimary"))
+        .accentColor((.brandPrimary))
         
     }
     
