fae5d41fb8d1faa286edeae6f82ddcc515a3e6c5
Nicolas Rios <nickrios33@yahoo.com>
2021-02-20 23:19:59 -0800

    fixed bug

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    
+    var order = Order()
+    
+    var body: some Scene {
+        WindowGroup {
+            AppetizerTabView().environmentObject(order)
+        }
+    }
+}
